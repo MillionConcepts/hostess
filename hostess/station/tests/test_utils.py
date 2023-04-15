e33eeb0489c3bb365e6f8abc2a849c33e349dd77
michael <mstclair@millionconcepts.com>
2023-04-15 12:31:54 -0400

    comments

--- hostess/station/tests/test_utils.py
+++ hostess/station/tests/test_utils.py
@@ -33,22 +33,31 @@ def test_tcp_server():
     test the hostess.talkie server by briefly hammering it from 7 processes
     and checking recorded input for validity
     """
+    # launch the server with 7 threads and a 1-ms poll rate
     host, port, n_threads, interval = "localhost", 11129, 4, 0.001
     server = launch_tcp_server(host, port, n_threads, interval)
+    # record time
     watch = Stopwatch()
     watch.start()
+    # number of processes to hit it with
     n_workers = 7
     pool, results, messages, exceptions, res = Pool(n_workers), {}, {}, {}, {}
-    args = [host, port, 5 * 1024 * 1024, 5, 0, None]
+    # host, port, size of garbage messages, client timeout in seconds
+    args = [host, port, 5 * 1024 * 1024, 5]
+    # send garbage all at once
     for i in range(7):
+        # add a tag so we can tell which was which at the end
         tag = f"{i}\nSTART\n".encode("ascii")
         results[i] = pool.apply_async(send_randbytes, args, {"header": tag})
     pool.close()
     pool.join()
+    # how long did that take?
     print(watch.clickpeek())
+    # unpack results
     for i, r in results.items():
         messages[i], exceptions[i], res[i] = r.get()
     pool.terminate()
+    # check received against sent messages
     for rec in server["data"]:
         message = rec["content"]
         if len(message) == 0:
@@ -57,4 +66,5 @@ def test_tcp_server():
             re.search(r"\d+", message[:4].decode("ascii")).group()
         )
         assert message == messages[message_id]
+    # close the socket and terminate the listeners so the test will terminate
     server['kill']()
