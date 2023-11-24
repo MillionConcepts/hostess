9d53efdbd82d8df8a713cd54bbc7b8491e4b86b4
m-stclair <mstclair@millionconcepts.com>
2023-11-24 01:44:02 -0500

    explain some test utilz

--- hostess/tests/utilz.py
+++ hostess/tests/utilz.py
@@ -1,28 +1,34 @@
+"""utility functions for hostess unit tests."""
+
 from typing import Any, Callable
 
 from cytoolz import identity
 
 
 # NOTE: this is used as a target by a test that only references it by name,
-#  so even though it isn't directly called anywhere in the library, don't
+#  so even though it isn't explicitly called anywhere in the library, don't
 #  remove it.
 def return_this(this):
+    """return this."""
     return this
 
 
 def defwrap(defline: str, source: str) -> str:
-    """properly indent source code under a def statement"""
+    """properly indent source code under a def statement."""
     indented = "\n".join([f"\t{line}" for line in source.splitlines()])
     return f"{defline}:\n{indented}"
 
 
 def pointlessly_nest(obj: Any, func: Callable = identity) -> Any:
-    """wrap a unary function call for no reason"""
+    """wrap a unary function call for no real reason."""
     return func(obj)
 
 
-def segfault(max_tries=100):
-    """attempt to segfault the calling process."""
+def segfault(max_tries: int = 100):
+    """
+    attempt to segfault the calling process. cannot be used carefully, but use
+    only with measured recklessness.
+    """
     from hostess.profilers import di
 
     for i in range(max_tries):
@@ -32,10 +38,18 @@ def segfault(max_tries=100):
 
 class Aint:
     """
-    acts arithmetically like obj, but ain't actually it.
-    note: not reliably monadic for all primitive types, e.g. bool
+    acts arithmetically like some other object, but ain't actually it. useful
+    for convincing the garbage collector to track something that is basically
+    a primitive or testing functions that are meant to distinguish one object
+    from another in an unduckish fashion.
+
+    note: not reliably monadic for all primitive types, e.g. `bool`.
     """
-    def __init__(self, obj):
+    def __init__(self, obj: Any):
+        """
+        Args:
+            obj: object this ain't.
+        """
         if isinstance(obj, Aint):
             self.__obj = obj.__obj
         else:
