52b18a846244d9f098ae15a571ee623250eebbdf
Sierra Brown <sierra@millionconcepts.com>
2023-11-28 15:57:48 -0500

    bump version number for release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="hostess",
-    version="0.6.1b",
+    version="0.7.0",
     description="intuitive admin library",
     author="Million Concepts",
     author_email="mstclair@millionconcepts.com",
